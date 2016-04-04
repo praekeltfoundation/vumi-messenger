596e1451052c1783e36e67635fbd48435e2e6a5d
Simon de Haan <simon@praekeltfoundation.org>
2016-04-04 17:55:03 +0200

    respond to /

--- vxmessenger/webapp/urls.py
+++ vxmessenger/webapp/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from .views import privacy
+from .views import privacy, home
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^privacy/', privacy),
+    url(r'^$', home),
 ]
