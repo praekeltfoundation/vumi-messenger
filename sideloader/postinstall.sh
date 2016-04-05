ec532f4ef69225e19ab8564a2100a17979448004
Simon de Haan <simon@praekeltfoundation.org>
2016-04-05 13:40:38 +0200

    remove pip install from post-install

--- sideloader/postinstall.sh
+++ sideloader/postinstall.sh
@@ -1,6 +1,3 @@
-# Post install scripts for sideloader
-${PIP} install -e $INSTALLDIR/$NAME
-
 export DJANGO_SETTINGS_MODULE=vxmessenger.webapp.settings
 django-admin migrate --no-input
 django-admin collectstatic --no-input
